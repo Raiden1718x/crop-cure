85d12131df08c5f492c92a2346902b1c312ab9c0
Raiden1718x <akshatahanas@gmail.com>
2025-08-28 05:51:00 -0600

    react

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden flex items-center justify-end bg-green-200">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold font-quicksand mr-6 sm:mr-25 mb-20 text-green-700">
+        CROPCURE
+      </h1>
+    </div>
+  )
+}
+
+export default Hero
